c42e2480a1ef0519be40228d651e91050539ee87
David Hilera <david.hilera@hotmail.com>
2025-04-14 18:57:59 -0600

    chore 🧹 remove unused assets and add prisma prod cmd
    Removed unused image assets and added a Prisma command
    for deploying to production environments.
    
    - Deleted unused image files to reduce clutter and optimize repo size
    - Updated `.gitignore` to ignore `diff.txt`
    - Added `prisma generate` command for production to `package.json` or scripts

--- src/app/platform/profile/page.tsx
+++ src/app/platform/profile/page.tsx
@@ -24,48 +24,6 @@ const ProfilePage = async () => {
 
   return (
     <ProfileClient user={user} />
-
-    // <div className='w-full'>
-    //   <Card className='max-w-sm mx-auto'>
-    //     <CardHeader className='text-center'>
-    //       <h1 className={`${titleFont.className} text-2xl mb-3`}>Profile</h1>
-    //     </CardHeader>
-
-    //     <CardContent className='flex justify-center'>
-    //       <Avatar className="bg-gray-200 text-gray-600 h-28 w-28 rounded-full">
-    //         <AvatarImage src={userImage} />
-    //         <AvatarFallback>Avatar</AvatarFallback>
-    //       </Avatar>
-    //     </CardContent>
-
-    //     <CardContent >
-    //       <div className='flex justify-end'>
-    //         <Button asChild variant='outline' size='sm' className='gap-1'>
-    //           <Link href={`/platform/profile/${user.id}?redirectTo=profile`} className='text-[10px]'>
-    //             <IoPencil />
-    //             <span className='hidden sm:flex'>Edit</span>
-    //           </Link>
-    //         </Button>
-    //       </div>
-    //     </CardContent>
-
-    //     <CardContent >
-    //       <p><span className='font-semibold'>Name:</span> {userName}</p>
-    //       <p><span className='font-semibold'>E-mail:</span> {userMail}</p>
-    //       <p><span className='font-semibold'>Phone number:</span> {userPhoneNumber}</p>
-    //     </CardContent>
-
-    //     <CardFooter className='space-y-2 flex-col'>
-    //       <div className='flex flex-col gap-4 justify-center mt-10 w-full'>
-    //         <Button>
-    //           Algún otro botón
-    //         </Button>
-    //       </div>
-    //       <ButtonLogout className='w-full' />
-    //     </CardFooter>
-
-    //   </Card>
-    // </div>
   )
 }
 
